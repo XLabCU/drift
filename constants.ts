28a499db15c15db5922376d35cf77b14ead6d331
Shawn Graham <shawn.graham@carleton.ca>
2026-02-05 13:11:27 -0500

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -1,6 +1,6 @@
 
 export const SPECTRAL_VOICES = ['Charon', 'Kore', 'Fenrir', 'Puck', 'Zephyr'];
-export const DRIFT_RADIUS_METERS = 5000;
+export const DRIFT_RADIUS_METERS = 2000;
 export const WIKI_API_ENDPOINT = "https://en.wikipedia.org/w/api.php";
 export const BASE_PATH = (window as any).BASE_PATH || 'https://xlabcu.github.io/drift';
 
